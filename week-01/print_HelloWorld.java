83fdfe46d8d65e7dfc2d9ba43d76891adb00197f
PhatTT28 <phat12340987@gmail.com>
2026-05-26 16:11:13 +0700

    Add print Hello World excersise

---
+++ week-01/print_HelloWorld.java
@@ -0,0 +1,17 @@
+// Nhập vào 1 số nguyên x(1 <= 10^9) và in ra 3 dòng:
+/*
+Dòng 1 là số x vừa nhập từ bàn phím
+Dòng 2 in ra dòng chữ "Hello World !"
+Dòng 3 in ra "Java programming !"
+*/
+import java.util.Scanner;
+
+public class print_HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+        System.out.print("Hello World !\n");
+        System.out.println("Java programming !");
+    }
+}

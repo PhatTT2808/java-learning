753b800be0ad76ea85206116e41717f103ba1353
PhatTT28 <phat12340987@gmail.com>
2026-05-27 10:47:25 +0700

    Add last digit and last two digits

---
+++ week-01/last_digit_and_last_two_digits.java
@@ -0,0 +1,24 @@
+/*
+Cho nguyên dương N, sử dụng phép chia lấy dư để tìm chữ số cuối cùng và 2 chữ số cuối cùng của N(100<=N<=10^16)
+Đầu vào:
+1 dòng chứa 1 số nguyên dương N
+Đầu ra:
+Dòng 1 chứa chữ số cuối cùng của N
+Dòng 2 in ra 2 chữ số cuối cùng nếu 2 chữ số cuối cùng của N có số 0 ở đầu thì bỏ số 0 đó đi và chọn số cuối cùng
+*/
+
+import java.util.Scanner;
+
+public class last_digit_and_last_two_digit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long N = sc.nextLong();
+        // chữ số cuối cùng của N
+        System.out.println(N%10);
+        // 2 chữ số cuối cùng của N
+        System.out.println(N%100);
+
+        sc.close();
+    }
+}    

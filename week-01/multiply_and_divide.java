ca0fcb52504a6328175cb896333bc9418bc0cc34
PhatTT28 <phat12340987@gmail.com>
2026-05-29 22:59:53 +0700

    multiply and divide

---
+++ week-01/multiply_and_divide.java
@@ -0,0 +1,28 @@
+/*
+Cho số nguyên N in ra các kết quả sau
+Đầu vào
+1 số nguyên dương N (1<=N<=10^9)
+Đầu ra
+Dòng 1 2 lần N
+Dòng 2 10 lần N
+Dòng 3 phép chia nguyên N với 2
+Dòng 4 phép chia lấy phàn thập phân N với 2, 3 chữ số sau dấu phẩy
+*/
+
+import java.util.Scanner;
+
+public class multiply_and_divide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(2*N);
+        System.out.println(10*N);
+        System.out.println(N/2);
+        System.out.printf("%.3f", (double)N/2);
+
+
+        sc.close();
+    }
+}

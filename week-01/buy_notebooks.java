373b584620b9f9f8bbfb402aa31d1950ed4e2215
PhatTT28 <phat12340987@gmail.com>
2026-05-31 23:26:54 +0700

    Add  buy notebook

--- week-01/buy_notebooks.java
+++ week-01/buy_notebooks.java
@@ -1,5 +1,5 @@
 /*
-1 người đi mua vở có N đồng, mỗi quyên vở x đồng, anh ta mua được tối đa bao nhiêu quyển vở?
+1 người đi mua vở có N đồng, mỗi quyển vở x đồng, anh ta mua được tối đa bao nhiêu quyển vở?
 Đầu vào
 2 số nguyên dương N, x (1<=N<=10^12,1<=x<=1000)
 Đầu ra
@@ -14,7 +14,7 @@ public class buy_notebooks {
         Scanner sc = new Scanner(System.in);
 
         Long N = sc.nextLong(), x = sc.nextLong();
-        System.out.println("Số quyển vở mua được là " + N/x + "!");
+        System.out.println("Số quyển vở tối đa mua được là " + N/x + "!");
 
         sc.close();
     }

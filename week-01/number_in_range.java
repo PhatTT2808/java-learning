e77059bf47ac3bf7855c69021b4ec420375f10d9
PhatTT28 <phat12340987@gmail.com>
2026-05-31 22:24:51 +0700

    Add number_in_range

---
+++ week-01/number_in_range.java
@@ -0,0 +1,20 @@
+/*
+Cho 2 số a và b, in ra số lượng số nguyên tính từ a đến b
+Đầu vào
+2 số nguyên dương a, b (1<=a,b<=10^12)
+Đầu ra
+Số lượng số nguyên tình từ a đến b
+*/
+
+import java.util.Scanner;
+
+public class number_in_range {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long a = sc.nextLong(), b = sc.nextLong();
+        System.out.println(b - a + 1);
+
+        sc.close();
+    }
+}
